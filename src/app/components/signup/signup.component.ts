2f1e46843e95dc1a0bf31de5a8053037be31e230
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-04-15 12:29:34 +0100

    Add signup form to signup component

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/services/login.service';
+
+interface Signup {
+  username: string;
+  password: string | undefined;
+  name: string | undefined;
+  jobTitle: string | undefined;
+  bio: string | undefined;
+}
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.less']
+  styleUrls: ['./signup.component.less'],
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+  constructor(private loginService: LoginService) {}
+  loginSuccess = this.loginService.state.success;
+  errors: { msg: string }[] = [];
+  signup: Signup = {
+    username: '',
+    password: '',
+    name: '',
+    jobTitle: '',
+    bio: '',
+  };
+  ngOnInit(): void {}
 
+  onSubmit(signupForm: { valid: any }) {
+    if (signupForm.valid) {
+      this.errors = [];
+      console.log('Form valid!', this.signup);
+    } else {
+      this.errors.push({ msg: 'Form Invalid!' });
+      console.log('Form Invalid!');
+    }
+  }
 }
