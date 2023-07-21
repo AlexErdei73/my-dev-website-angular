5567f572be298563d53f081094f0f9e82a4d463b
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-07-21 20:40:18 +0100

    Fix author component

--- src/app/components/author/author.component.ts
+++ src/app/components/author/author.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { User } from 'src/app/model/user';
-import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-author',
@@ -9,9 +8,9 @@ import { LoginService } from 'src/app/services/login.service';
 })
 export class AuthorComponent implements OnInit {
   @Input() author!: User;
-  username = this.loginService.state.user.username;
-  constructor(private loginService: LoginService) {}
+  username!: string;
   ngOnInit(): void {
     this.author = this.author as User;
+    this.username = this.author.username;
   }
 }
