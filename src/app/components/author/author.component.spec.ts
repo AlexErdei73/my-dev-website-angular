4614e99cc39358a21f2afde2a8273e608fb07ecd
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-07-21 20:40:41 +0100

    Fix testing of author component

--- src/app/components/author/author.component.spec.ts
+++ src/app/components/author/author.component.spec.ts
@@ -1,23 +1,33 @@
-import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { LoginComponent } from '../login/login.component';
-import { PostsComponent } from '../posts/posts.component';
-
+import { User } from 'src/app/model/user';
 import { AuthorComponent } from './author.component';
 
 describe('AuthorComponent', () => {
   let component: AuthorComponent;
   let fixture: ComponentFixture<AuthorComponent>;
+  let author: User;
+
+  author = {
+    _id: '63ab6c333bbf8653df7e1175',
+    username: 'test username',
+    password: '',
+    hash: '',
+    isAdmin: false,
+    jobTitle: 'test job',
+    bio: 'test bio',
+    name: 'test name',
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AuthorComponent],
-      imports: [HttpClientModule],
-      providers: [LoginComponent, PostsComponent],
+      //imports: [HttpClientModule],
+      //providers: [LoginComponent, PostsComponent],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AuthorComponent);
     component = fixture.componentInstance;
+    component.author = author;
     fixture.detectChanges();
   });
 
